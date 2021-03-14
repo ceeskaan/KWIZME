923e00716d6a9965537d62882950e2f815fb40b6
stanlochten <stanlochtenberg@gmail.com>
2021-03-15 00:08:59 +0100

    some styling

--- app.py
+++ app.py
@@ -4,10 +4,13 @@ from pipelines import pipeline
 
 app = Flask(__name__)
 
+print("Loading model...")
 model = pipeline("question-generation")
+print("Model loaded!")
 
 @app.route('/')
 def index():
+
 	return render_template("index.html", data="hey")
 
 
